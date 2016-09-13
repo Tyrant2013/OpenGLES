b47f86a3f7eae71213a04faa3095106957565ef0
Zhuang Xiaowei <zhuangxiaowei_dev@163.com>
2016-09-13 10:15:41 +0800

    add draw rectangle.

--- OpenGLES/ViewController.m
+++ OpenGLES/ViewController.m
@@ -8,10 +8,20 @@
 
 #import "ViewController.h"
 
+typedef NS_ENUM(NSUInteger, DrawType) {
+    DrawTypeTriangle,
+    DrawTypeRectangle,
+    DrawTypeCube
+};
+
 typedef struct {
     GLKVector3 positionCoords;
     GLKVector2 textureCoords;
 } SceneVertex;
+
+typedef struct {
+    GLKVector3 positionCoords;
+} RectangleVertex;
 
 //static const SceneVertex vertices[] = {
 //    {-0.5f, -0.5f, 0.0f},
@@ -25,7 +35,16 @@ static const SceneVertex verticesWithTexture[] = {
     {-0.5f,  0.5f, 0.0f, 0.0f, 1.0f}
 };
 
+static const RectangleVertex verticesForRectangle[] = {
+    {-0.5f, -0.5f, 0.0f},
+    { 0.5f, -0.5f, 0.0f},
+    { 0.5f,  0.5f, 0.0f},
+    {-0.5f,  0.5f, 0.0f},
+};
+
 @interface ViewController ()
+
+@property (nonatomic, assign) DrawType              drawType;
 
 @end
 
@@ -35,22 +54,10 @@ static const SceneVertex verticesWithTexture[] = {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    GLKView *view = (GLKView *)self.view;
-    view.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
-    [EAGLContext setCurrentContext:view.context];
+    self.drawType = DrawTypeRectangle;
     
-    self.baseEffect = [[GLKBaseEffect alloc] init];
-    self.baseEffect.useConstantColor = GL_TRUE;
-    self.baseEffect.constantColor = GLKVector4Make(1.0f, 1.0f, 1.0f, 1.0f);
-    
-    glGenBuffers(1, &vertexBufferID);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesWithTexture), verticesWithTexture, GL_STATIC_DRAW);
-    
-    UIImage *image = [UIImage imageNamed:@"leaves.gif"];
-    GLKTextureInfo *info = [GLKTextureLoader textureWithCGImage:image.CGImage options:nil error:NULL];
-    self.baseEffect.texture2d0.name = info.name;
-    self.baseEffect.texture2d0.target = info.target;
+    [self __initContext];
+    [self __init__];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -58,37 +65,10 @@ static const SceneVertex verticesWithTexture[] = {
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - GLKViewDelegate
+
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
-//    [self.baseEffect prepareToDraw];
-//    
-//    glClear(GL_COLOR_BUFFER_BIT);
-//    
-//    GLsizeiptr offset = offsetof(SceneVertex, positionCoords);
-//    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-//    glEnableVertexAttribArray(GLKVertexAttribPosition);
-//    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), NULL + offset);
-//    
-//    offset = offsetof(SceneVertex, textureCoords);
-//    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-//    glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
-//    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), NULL + offset);
-//    
-//    glDrawArrays(GL_TRIANGLES, 0, 3);
-    
-    [self.baseEffect prepareToDraw];
-    
-    glClear(GL_COLOR_BUFFER_BIT);
-    GLsizeiptr offset = offsetof(SceneVertex, positionCoords);
-//    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-    glEnableVertexAttribArray(GLKVertexAttribPosition);
-    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), NULL + offset);
-    
-    offset = offsetof(SceneVertex, textureCoords);
-//    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-    glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
-    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), NULL + offset);
-    
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    [self __draw__];
 }
 
 - (void)dealloc {
@@ -103,6 +83,108 @@ static const SceneVertex verticesWithTexture[] = {
     
 //    ((GLKView *)self.view).context = nil;
     [EAGLContext setCurrentContext:nil];
+}
+
+#pragma mark - Methods
+
+- (void)__init__ {
+    switch (_drawType) {
+        case DrawTypeTriangle:
+            [self __initDrawTriangle];
+            break;
+        case DrawTypeRectangle:
+            [self __initDrawRectangle];
+            break;
+        case DrawTypeCube:
+            [self __initDrawCube];
+            break;
+    }
+}
+
+- (void)__initContext {
+    GLKView *view = (GLKView *)self.view;
+    view.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    [EAGLContext setCurrentContext:view.context];
+}
+
+- (void)__draw__ {
+    switch (_drawType) {
+        case DrawTypeTriangle:
+            [self __drawTriangle];
+            break;
+        case DrawTypeRectangle:
+            [self __drawRectangle];
+            break;
+        case DrawTypeCube:
+            [self __drawCube];
+            break;
+    }
+}
+
+#pragma mark - Draw Triangle
+
+- (void)__initDrawTriangle {
+    
+    glGenBuffers(1, &vertexBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesWithTexture), verticesWithTexture, GL_STATIC_DRAW);
+    
+    self.baseEffect = [[GLKBaseEffect alloc] init];
+    self.baseEffect.useConstantColor = GL_TRUE;
+    self.baseEffect.constantColor = GLKVector4Make(1.0f, 1.0f, 1.0f, 1.0f);
+    
+    UIImage *image = [UIImage imageNamed:@"leaves.gif"];
+    GLKTextureInfo *info = [GLKTextureLoader textureWithCGImage:image.CGImage options:nil error:NULL];
+    self.baseEffect.texture2d0.name = info.name;
+    self.baseEffect.texture2d0.target = info.target;
+}
+
+- (void)__drawTriangle {
+    [self.baseEffect prepareToDraw];
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    GLsizeiptr offset = offsetof(SceneVertex, positionCoords);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), NULL + offset);
+    
+    offset = offsetof(SceneVertex, textureCoords);
+    glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
+    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), NULL + offset);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    
+}
+
+#pragma mark - Draw Rectangle 
+
+- (void)__initDrawRectangle {
+    glGenBuffers(1, &vertexBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesForRectangle), verticesForRectangle, GL_STATIC_DRAW);
+    
+    self.baseEffect = [[GLKBaseEffect alloc] init];
+    self.baseEffect.useConstantColor = GL_TRUE;
+    self.baseEffect.constantColor = GLKVector4Make(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+- (void)__drawRectangle {
+    [self.baseEffect prepareToDraw];
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(RectangleVertex), NULL);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+}
+
+#pragma mark - Draw Cube
+
+- (void)__initDrawCube {
+    
+}
+
+- (void)__drawCube {
+    
 }
 
 @end
